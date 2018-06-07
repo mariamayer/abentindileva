188ea264b099aa65233bec4765add7ab34aafc1f
Maria Mayer <mayermaru@gmail.com>
2018-06-07 11:22:30 -0300

    Menu agregar clase con scroll

--- functions.php
+++ functions.php
@@ -130,6 +130,8 @@ function abentindileva_scripts() {
 
     wp_enqueue_script( 'abentindileva-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
+    wp_enqueue_script( 'abentindileva-main', get_template_directory_uri() . '/js/main.js', array(), '20151215', true );
+
 	wp_enqueue_script( 'abentindileva-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
